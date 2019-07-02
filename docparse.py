ad8eb5fc54a08a73d52e052d0e3349a30879856e
Abshir Mohamed <abshir24@Abshirs-MBP.T-mobile.com>
2019-07-01 20:02:08 -0700

    final changes made

--- docparse.py
+++ docparse.py
@@ -3,13 +3,14 @@
 import xlrd
 
 # for those using a mac without excel the file extensions should be xlsx instead of xls
-book = xlrd.open_workbook('FILENAME.xls')
+# Download the file from teams and place it in this directory
+# MAKE SURE THE FILE IS IN THIS DIRECTORY!
+filename = xlrd.open_workbook('usernames.xlsx')
 
 usernames = []
 
-for sheet in book.sheets():  
+for sheet in filename.sheets():  
     for row in range(sheet.nrows):  
         if row > 0: 
             usernames.append(sheet.row(row)[2].value)
        
-
--- githubbot.py
+++ githubbot.py
@@ -3,11 +3,13 @@ import requests
 import json
 #importing the docparse file so that usernames can be retrieved
 import docparse
+import time
 
-username = 'INSERT USERNAME'
+# ADD GITHUB USERNAME
+username = 'USERNAME'
 
-# from https://github.com/settings/tokens
-token = 'INSERT TOKEN'
+# Add API token from https://github.com/settings/tokens
+token = 'API-TOKEN'
 
 
 # # create a re-usable session object with the user credits in-built
@@ -18,14 +20,21 @@ gh_session.auth = (username, token)
 # this is needed when passing in the request header on the api call
 h = {'Content-Length' : '0'}
 
+users = docparse.usernames
 
 # iterate through the list of usernames from the docparse file
-for user in docparse.usernames:
+for user in users:
+    # this line allows us to follow the respected amount of users without throwing any red flags to github
+    # idicating that there is something attacking their website
+    time.sleep(3)
     #this url will be used to make the PUT request that will follow the user from your account
     follow_url = 'https://api.github.com/user/following/'+user
     # this is the function that makes the PUT request to follow the user
     # an empty response indicates that the follow was successful
     follow = gh_session.put(follow_url,headers = h).text
     print(follow)
+
+
+
 
 
